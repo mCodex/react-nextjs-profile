7a217bc5528a5235ca341875c58333bd00ea1ddf
Mateus Andrade <mat.andrade@live.com>
2018-04-04 20:58:22 -0300

    injecting styles from styled-components into html header

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class RootDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags }; // return styles collected
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
