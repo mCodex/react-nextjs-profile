5a1c8c17dc680f5d30b358ccf2e0ae55cf3cc7e3
Victor Pedrazzi <vspedrazzi@gmail.com>
2018-04-04 21:08:09 -0300

    #3 Add Google Analytics feature

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import Head from 'next/head';
+import ReactGA from 'react-ga';
 
 import startI18n from '../tools/startI18n';
 import { getTranslation } from '../tools/translationHelpers';
@@ -9,7 +10,9 @@ import Interests from '../components/Interests';
 import AvatarContainer from '../components/AvatarContainer';
 import Skills from '../components/Skills';
 
-export default class Homepage extends Component {
+import withData from '../hocs/DataProvider';
+
+class Homepage extends Component {
   static async getInitialProps({ req }) {
     //Getting language from user's browser
     const lang = req.headers['accept-language'].substring(0, 2);
@@ -26,6 +29,14 @@ export default class Homepage extends Component {
   constructor(props) {
     super(props);
     this.i18n = startI18n(props.translations, this.props.lang);
+
+    // Google Analytics setup
+    const { gaTrackingId } = this.props.data;
+    ReactGA.initialize(gaTrackingId);
+  }
+
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname, { title: 'Profile' });
   }
 
   render() {
@@ -45,3 +56,5 @@ export default class Homepage extends Component {
     );
   }
 }
+
+export default withData(Homepage);
